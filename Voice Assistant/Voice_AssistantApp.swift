f698f136f2bfc65d6010eec0153d6cec732ec0ee
kkarlo <kkaragic@gmail.com>
2023-03-06 19:04:23 +0100

    Initial Commit

---
+++ Voice Assistant/Voice_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Voice_AssistantApp.swift
+//  Voice Assistant
+//
+//  Created by Karlo Karagić on 06.03.2023..
+//
+
+import SwiftUI
+
+@main
+struct Voice_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
